ff41f11782551b1d2275255613dd252722e7a343
Gonzalo Alvarez <gonzaloab@gmail.com>
2017-01-07 00:02:07 -0800

    Test

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@
 SELF_PATH="$(cd "$(dirname "$0")" && pwd)"
 TARGET_BASHRC="$HOME/.bashrc"
 HOSTNAME_FILE="$HOME/.hostname"
+TMUX_CONF="$HOME/.tmux.conf"
 
 function _die {
     echo "$@" >&2
     exit 1
 }
 
-function _build_env {
+function _build_bash {
     echo "Building bash environment"
     echo -e "#!/bin/bash\n" > $TARGET_BASHRC
     for i in $SELF_PATH/bash/functions/*.sh; do
@@ -29,4 +30,5 @@ if [ "$1" ]; then
     echo "$1" > $HOSTNAME_FILE
 fi
 
-_build_env
+_build_bash
+cp -v "$SELF_PATH/tmux/tmux.conf" "$TMUX_CONF"

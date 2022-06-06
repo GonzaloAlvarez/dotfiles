37f975099971ad3625344e64e740680c5e5414f7
Gonzalo Alvarez <gonzaloab@gmail.com>
2022-06-05 18:48:56 -0700

    Fix extension lookup when there is none

--- bashrc/700Includes.sh
+++ bashrc/700Includes.sh
@@ -2,6 +2,8 @@ if [ -f "$HOME/.gear/commands.sh" ]; then
     source "$HOME/.gear/commands.sh"
 fi
 
-for bashext in $HOME/.bashrc.ext.*; do
-    source "$bashext"
-done
+if ls $HOME/.bashrc.ext.* 1>/dev/null 2>&1; then
+    for bashext in $HOME/.bashrc.ext.*; do
+        source "$bashext"
+    done
+fi

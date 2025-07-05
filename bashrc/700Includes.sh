e143be6ae6d45bbb4da9c358ab42fcc679c9a988
Gonzalo Alvarez <gonzaloab@gmail.com>
2025-07-05 18:45:08 -0400

    Added asdf to the path if it exists in the system

--- bashrc/700Includes.sh
+++ bashrc/700Includes.sh
@@ -2,6 +2,10 @@ if [ -f "$HOME/.gear/commands.sh" ]; then
     source "$HOME/.gear/commands.sh"
 fi
 
+if [ -f "$(brew --prefix asdf)/libexec/asdf.sh" ]; then
+    source "$(brew --prefix asdf)/libexec/asdf.sh"
+fi
+
 if ls $HOME/.bashrc.ext.* 1>/dev/null 2>&1; then
     for bashext in $HOME/.bashrc.ext.*; do
         source "$bashext"

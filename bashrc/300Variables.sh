81dad0c70dbf4998d5a195c0cb3beb7df6877bc7
Gonzalo Alvarez <gonzaloab@gmail.com>
2025-08-02 23:08:00 -0400

    Added asdf for linux in the path

--- bashrc/300Variables.sh
+++ bashrc/300Variables.sh
@@ -30,6 +30,10 @@ if [ -d $HOME/bin ]; then
     export PATH="$HOME/bin:$PATH"
 fi
 
+if [ -d "$HOME/.asdf/shims" ]; then
+    export PATH="$HOME/.asdf/shims:$PATH"
+fi
+
 # Files will be created with these permissions:
 # files 644 -rw-r--r-- (666 minus 022)
 # dirs  755 drwxr-xr-x (777 minus 022)
